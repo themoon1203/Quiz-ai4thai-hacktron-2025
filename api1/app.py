89b258456d996eb713750a07a9ae268f257d2200
themoon1203 <thitipan1653@gmail.com>
2025-07-20 22:29:49 +0700

    add logs

--- api1/app.py
+++ api1/app.py
@@ -1,5 +1,30 @@
-from flask import Flask, jsonify, request
 import requests
+from flask import Flask, jsonify, request
+from colorlog import ColoredFormatter
+from logging import StreamHandler, basicConfig, getLogger, INFO
+
+
+getLogger('').handlers.clear()
+
+basicConfig(level=INFO)
+logger = getLogger('api1')
+
+logger.handlers.clear()
+logger.propagate = False
+
+handler = StreamHandler()
+formatter = ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(name)s:%(message)s',
+    log_colors={
+        'INFO': 'blue',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+handler.setFormatter(formatter)
+logger.handlers = [handler]
+
 
 app = Flask(__name__)
 
@@ -9,7 +34,7 @@ def api1():
     if request.method == 'POST':
         data = request.json
 
-    print('API1: Received request from User %s' % (
+    logger.info('API1: Received request from User %s' % (
         'with data: ' + str(data) if data else 'without data'
     ))
 
@@ -20,7 +45,7 @@ def api1():
     else:
         response = requests.get(target_url)
 
-    print('API1: Got response from API2: %s' % response.text)
+    logger.info('API1: Got response from API2: %s' % response.text)
 
     return jsonify({'answer': response.json().get('answer')})
 
--- api2/app.py
+++ api2/app.py
@@ -1,4 +1,28 @@
 from flask import Flask, jsonify, request
+from colorlog import ColoredFormatter
+from logging import StreamHandler, basicConfig, getLogger, INFO
+
+
+getLogger('').handlers.clear()
+
+basicConfig(level=INFO)
+logger = getLogger('api2')
+
+logger.handlers.clear()
+logger.propagate = False
+
+handler = StreamHandler()
+formatter = ColoredFormatter(
+    '%(log_color)s%(levelname)s:%(name)s:%(message)s',
+    log_colors={
+        'INFO': 'yellow',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red,bg_white',
+    }
+)
+handler.setFormatter(formatter)
+logger.handlers = [handler]
 
 app = Flask(__name__)
 
@@ -8,7 +32,7 @@ def api2():
     if request.method == 'POST':
         data = request.json
 
-    print('API2: Received request from API1 %s' % (
+    logger.info('API2: Received request from API1 %s' % (
         'with data: ' + str(data) if data else 'without data'
     ))
 
